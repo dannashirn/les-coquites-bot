644e8b1090885ee98d00595e340e7bb5b01a2105
Ignacio Reyna <ignacio.reyna@despegar.com>
2018-05-18 16:00:45 -0300

    adding subte notifications via subscription

--- config/apis.js
+++ config/apis.js
@@ -8,5 +8,6 @@ module.exports = {
   chuckNorris: "https://api.chucknorris.io/jokes/random",
   todoList: "https://api.myjson.com/bins/1849jq",
   dolar: "http://ws.geeklab.com.ar/dolar/get-dolar-json.php",
-  atr: "https://api.myjson.com/bins/pd72u"
+  atr: "https://api.myjson.com/bins/pd72u",
+  suscripcionSubte: "https://api.myjson.com/bins/l3ubm"
 };
