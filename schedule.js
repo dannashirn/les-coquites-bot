a60cb48778c389c6a6e61704f244a709fc11fc28
Ignacio Andres Reyna <30632437+ignacioreyna@users.noreply.github.com>
2018-06-28 15:35:39 -0300

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -3,7 +3,7 @@ var schedule = require("node-schedule");
 var apis = require("./config/apis");
 var bot = require("./bot");
 module.exports = schedule.scheduleJob("*/1 * * * *", () => {
-  request.get("https://botback.herokuapp.com/");
+  request.get("https://les-coquites-bot.herokuapp.com/");
 });
 
 module.exports = schedule.scheduleJob("*/3 * * * *", () => {
